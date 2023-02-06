6095b8711ce11695cc97225dcd48a81442844688
Jack Lach <jack.lach4@gmail.com>
2023-02-06 05:33:18 -0500

    test passing

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    const par1 = document.querySelector('#text');
+    par1.textContent = 'This is really cool!';
+})
+
+console.log('the DOM hasn\'t loaded yet');
